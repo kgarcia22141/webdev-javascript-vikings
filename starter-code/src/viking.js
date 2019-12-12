777ed8f01cd59a7e44d9d2a0a0f9f7e42f4461d2
Kenneth Garcia <22141@columbushs.com>
2019-12-12 08:56:56 -0500

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -13,7 +13,22 @@ receiveDamage(damage) {
  }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    }
+    receiveDamage(damage) {
+    return this.health = this.health - damage
+    }   
+    if(this.health <= 0)
+        return (' ${this.name} has recived ${damage} points of damage')
+    }
+    else{
+        return ('${this.name} has died in act of combat')
+    }
+    battleCry()
+}
 
 // Saxon
 class Saxon {}
